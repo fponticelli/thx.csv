748693187bb5f08904307f46a024c516365bbc2a
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-09-18 14:36:31 -0600

    implement it good

--- src/thx/csv/Dsv.hx
+++ src/thx/csv/Dsv.hx
@@ -6,9 +6,11 @@ class Dsv {
   public static function decode(dsv : String, options : DsvDecodeOptions) : Array<Array<String>> {
     if(null == options.quote) options.quote = '"';
     if(null == options.escapedQuote) options.escapedQuote = options.quote == '"' ? '""' : '\\${options.quote}';
-    var result = parse(dsv, options.delimiter, options.quote, options.escapedQuote);
-    if(options.trimmed) {
     if(null == options.trimValues) options.trimValues = false;
+    if(null == options.trimEmptyLines) options.trimEmptyLines = true;
+    if(options.trimEmptyLines)
+      dsv = dsv.trimChars('\n\r');
+    var result = dsv == "" ? [] : parse(dsv, options.delimiter, options.quote, options.escapedQuote);
     if(options.trimValues) {
       for(row in result)
         for(i in 0...row.length)
@@ -32,7 +34,97 @@ class Dsv {
   }
 
   static function parse(s : String, delimiter : String, quote : String, escapedQuote : String) : Array<Array<String>> {
-    var result = [];
+    var result = [],
+        pos = 0,
+        len = s.length,
+        delimiterLength = delimiter.length,
+        quoteLength = quote.length,
+        escapedQuoteLength = escapedQuote.length,
+        buffer = new StringBuf(),
+        row = null;
+
+    function pushCell() {
+      if(row == null) {
+        row = [];
+      }
+      row.push(buffer.toString());
+      buffer = new StringBuf();
+    }
+
+    function pushBuffer(char : String) {
+      buffer.add(char);
+    }
+
+    function pushRow() {
+      if(null == row) {
+        row = [];
+      } else {
+        result.push(row);
+        row = [];
+      }
+    }
+
+    var loopWithinQuotes, loop;
+
+    loopWithinQuotes = function() {
+      while(pos < len) {
+        if(s.substring(pos, pos + escapedQuoteLength) == escapedQuote) {
+          pushBuffer(quote);
+          pos += escapedQuoteLength;
+        } else if(s.substring(pos, pos + quoteLength) == quote) {
+          pos += quoteLength;
+          var next = s.substring(pos, pos + 1);
+          while(next == " " || (delimiter != "\t" && next == "\t")) {
+            ++pos;
+            next = s.substring(pos, pos + 1);
+          }
+          loop();
+        } else {
+          pushBuffer(s.substring(pos, pos + 1));
+          ++pos;
+        }
+        //break; // TODO REMOVE
+      }
+    }
+    loop = function() {
+      var t;
+      while(pos < len) {
+        if(s.substring(pos, pos + quoteLength) == quote) {
+          if(buffer.length > 0) {
+            var s = buffer.toString().trim();
+            if(s.length > 0) {
+              continue;
+            }
+            buffer = new StringBuf();
+          }
+          pos += quoteLength;
+          loopWithinQuotes();
+        } else if(s.substring(pos, pos + delimiterLength) == delimiter) {
+          pushCell();
+          pos += delimiterLength;
+        } else {
+          t = s.substring(pos, pos + 2);
+          if(t == '\n\r' || t == '\r\n') {
+            pos += 2;
+            pushCell();
+            pushRow();
+            continue;
+          }
+          t = s.substring(pos, pos + 1);
+          if(t == '\n' || t == '\r') {
+            ++pos;
+            pushCell();
+            pushRow();
+            continue;
+          }
+          pushBuffer(s.substring(pos, pos + 1));
+          ++pos;
+        }
+      }
+    }
+    loop();
+    pushCell();
+    pushRow();
 
     return result;
   }
@@ -58,5 +150,6 @@ typedef DsvDecodeOptions = {
   delimiter : String,
   ?quote : String,
   ?escapedQuote : String,
-  ?trimmed : Bool
+  ?trimValues : Bool,
+  ?trimEmptyLines : Bool
 }

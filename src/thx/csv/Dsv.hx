942c0b7fa0368ce2fb644fa9ed0686b3bc05c822
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-09-18 09:57:18 -0600

    make some options optional

--- src/thx/csv/Dsv.hx
+++ src/thx/csv/Dsv.hx
@@ -2,23 +2,29 @@ package thx.csv;
 
 class Dsv {
   public static function decode(csv : String, options : DsvDecodeOptions) : Array<Array<String>> {
+    if(null == options.quote) options.quote = '"';
+    if(null == options.escapedQuote) options.escapedQuote = options.quote == '"' ? '""' : '\\${options.quote}';
+    if(null == options.trimmed) options.trimmed = false;
     return [];
   }
   public static function encode(data : Array<Array<String>>, options : DsvEncodeOptions) : String {
+    if(null == options.quote) options.quote = '"';
+    if(null == options.escapedQuote) options.escapedQuote = options.quote == '"' ? '""' : '\\${options.quote}';
+    if(null == options.newline) options.newline = '\n';
     return "";
   }
 }
 
 typedef DsvEncodeOptions = {
   delimiter : String,
-  quote : String,
-  escapedQuote : String,
-  newline : String
+  ?quote : String,
+  ?escapedQuote : String,
+  ?newline : String
 }
 
 typedef DsvDecodeOptions = {
   delimiter : String,
-  quote : String,
-  escapedQuote : String,
-  trimmed : Bool
+  ?quote : String,
+  ?escapedQuote : String,
+  ?trimmed : Bool
 }

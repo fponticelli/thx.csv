79f919f756c123508aa266ebd55780d5ce7637c2
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-09-22 22:16:55 -0600

    oops

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm thx.csv.zip
-zip -r thx.csv.zip hxml src test doc/ImportAll.hx extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
+zip -r thx.csv.zip hxml src test doc/ImportCsv.hx extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit thx.csv.zip
